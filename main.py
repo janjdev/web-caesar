379caa20f62954e1e4a362f6dd92cd76c58768ff
January Johnson <jpjansa@gmail.com>
2019-03-26 01:11:03 -0500

    attempted to make html string more readable per TA's request.

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from caesar import encrypt
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = "<!DOCTYPE html> <html> <head> <style> form {{ background-color: #eee; padding: 20px; margin: 0 auto; width: 540px; font: 16px sans-serif; border-radius: 10px;}} textarea{{margin: 10px 0; width: 540px; height: 120px;}} </style> </head> <body> <form method='post'><label for='rot'>Rotate by: </label><input id='rot' type='text' name='rot'/> <textarea name='mess'> {0} </textarea> <button type='submit'>Submit</button></form>  </body><script type='text/javascript' src='static/main.js'></script> </html>"
+form = """<!DOCTYPE html> <html> 
+<head> <style> form {{ background-color: #eee; padding: 20px; 
+margin: 0 auto; width: 540px; font: 16px sans-serif; 
+border-radius: 10px;}} textarea{{margin: 10px 0; width: 540px; 
+height: 120px;}} 
+</style> </head> 
+<body> 
+<form method='post'><label for='rot'>Rotate by: </label><input id='rot' type='text' name='rot'/> 
+<textarea name='mess'> {0} </textarea> 
+<button type='submit'>Submit</button></form>  
+</body>
+<script type='text/javascript' src='static/main.js'></script> 
+</html>"""
 
 @app.route("/")
 def index():
